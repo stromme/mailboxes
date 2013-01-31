2a69155d5b7fcf3fc4445cc72d3c401a218a5977
Josua Leonard <josua.leonard@gmail.com>
2013-01-31 17:19:07 +0700

    Refactoring and cleanup

--- mailboxes.js
+++ mailboxes.js
@@ -3,7 +3,6 @@
  * User: josua
  * Date: 1/14/13
  * Time: 2:57 PM
- * To change this template use File | Settings | File Templates.
  */
 
 /**
@@ -12,46 +11,17 @@
  * @param email_account
  */
 function email_list_template(email_account){
-  var email_tmpl =
-    '<div class="tier" style="display:none;" email-account="'+email_account+'">'+
-      '<header><h4>'+email_account+'</h4></header>'+
-      '<nav>'+
-        '<a class="action-password" href="#">'+
-          '<small><i class="icon-cog"></i>change password</small>'+
-        '</a>'+
-        '<a class="action-delete" href="#">'+
-          '<small><i class="icon-trash"></i>delete</small>'+
-        '</a>'+
-      '</nav>'+
-    '</div>';
-  return email_tmpl;
-}
-
-/**
- * Function for showing the spinner when in progress of deleting email
- */
-function show_delete_email_spinner(){
-  $("#delete-confirm .action-confirm").parent().prepend('<span id="mailboxes-deletemail-loader" class="button-side-loader"><span class="loader"></span></span>');
-  $('#mailboxes-deletemail-loader .loader').spin('medium-left', '#000000');
-  $('#mailboxes-deletemail-loader').fadeIn(300);
-}
-
-/**
- * Function for showing the spinner when in progress of changing email password
- */
-function show_change_password_spinner(){
-  $("#change-password .action-save").parent().prepend('<span id="mailboxes-changepass-loader" class="button-side-loader"><span class="loader"></span></span>');
-  $('#mailboxes-changepass-loader .loader').spin('medium-left', '#000000');
-  $('#mailboxes-changepass-loader').fadeIn(300);
-}
-
-/**
- * Function for showing the spinner when in progress of adding email
- */
-function show_add_email_spinner(){
-  $('<div id="mailboxes-loader"><div class="loader"></div></div>').insertBefore("#emails-container");
-  $('#mailboxes-loader .loader').spin('medium-left', '#000000');
-  $('#mailboxes-loader').slideDown(200);
+  return '<div class="tier" style="display:none;" email-account="'+email_account+'">'+
+    '<header><h4>'+email_account+'</h4></header>'+
+    '<nav>'+
+      '<a class="action-password" href="#">'+
+        '<small><i class="icon-cog"></i>change password</small>'+
+      '</a>'+
+      '<a class="action-delete" href="#">'+
+        '<small><i class="icon-trash"></i>delete</small>'+
+      '</a>'+
+    '</nav>'+
+  '</div>';
 }
 
 function add_new_email(new_email, new_pass, forwarding, success_callback, failed_callback){
@@ -61,29 +31,34 @@ function add_new_email(new_email, new_pass, forwarding, success_callback, failed
     'new_password': new_pass,
     'forwarding': forwarding
   };
-
-  show_add_email_spinner();
-  $.post(ajaxurl, data, function(response) {
+  var container = $('#emails-container');
+  var post = new AjaxPost(data, {
+    'spinner': new LoadingSpinner({
+      'reference_elm': container,
+      'insert_method': 'before',
+      'style_class': 'mailboxes-loader',
+      'loader_tag': 'div',
+      'spinner_tag': 'div',
+      'loader_style': ' ',
+      'spinner_style': ' '
+    })
+  },
+  // Ajax replied
+  function(response){
     var json_response = JSON.parse(response);
-    $('#mailboxes-loader').slideUp(200, function(){
-      $(this).remove();
-      if(json_response.status==1){
-        $("#emails-container").prepend(email_list_template(json_response.account_email));
-        if(typeof(success_callback=="function")) success_callback();
-      }
-      else {
-        if(typeof(failed_callback=="function")) failed_callback();
-        bootstrap_alert(json_response.status_message, 'error');
-      }
-    });
-  })
-  .error(function() {
-    $('#mailboxes-loader').fadeOut(200, function(){
-      $(this).remove();
-      bootstrap_alert("Connection error", 'error');
-      if(typeof(failed_callback)=="function") failed_callback();
-    });
+    if(json_response.status==1){
+      container.prepend(email_list_template(json_response.account_email));
+      if(typeof(success_callback=="function")) success_callback();
+    }
+    else {
+      //if(typeof(failed_callback=="function")) failed_callback();
+      bootstrap_alert(json_response.status_message, 'error');
+    }
+  },
+  function(){
+    if(typeof(failed_callback)=='function') failed_callback();
   });
+  post.doAjaxPost();
 }
 
 /**
@@ -98,28 +73,30 @@ function confirm_delete_email(email, success_callback, failed_callback){
     action: 'delete_email',
     'email': email
   };
-  // Show spinner while waiting
-  show_delete_email_spinner();
-  $.post(ajaxurl, data, function(response) {
+  var container = $("#delete-confirm");
+  var button = $(".action-confirm", container);
+  var post = new AjaxPost(data, {
+    'spinner': new LoadingSpinner({
+      'reference_elm': button,
+      'in_parent': true,
+      'insert_method': 'prepend'
+    })
+  },
+  // Ajax replied
+  function(response){
     var json_response = JSON.parse(response);
-    // Remove loading spinner before something else
-    $('#mailboxes-deletemail-loader').fadeOut(300, function(){
-      $(this).remove();
-      if(json_response.status==1){
-        if(typeof(success_callback)=='function') success_callback();
-      }
-      else {
-        bootstrap_alert(json_response.status_message, 'error');
-        if(typeof(failed_callback)=='function') failed_callback();
-      }
-    });
-  })
-  .error(function() {
-    $('#mailboxes-deletemail-loader').fadeOut(200, function(){
-      $(this).remove();
-      bootstrap_alert("Connection error", 'error');
-    });
+    if(json_response.status==1){
+      if(typeof(success_callback)=='function') success_callback();
+    }
+    else {
+      bootstrap_alert(json_response.status_message, 'error');
+      if(typeof(failed_callback)=='function') failed_callback();
+    }
+  },
+  function(){
+    if(typeof(failed_callback)=='function') failed_callback();
   });
+  post.doAjaxPost();
 }
 
 /**
@@ -132,31 +109,34 @@ function confirm_delete_email(email, success_callback, failed_callback){
  * @param failed_callback
  */
 function confirm_change_password(email, new_password, success_callback, failed_callback){
-  show_change_password_spinner();
   var data = {
     action: 'change_password',
     'email': email,
     'password': new_password
   };
-  $.post(ajaxurl, data, function(response) {
+  var container = $("#change-password");
+  var button = $(".action-save", container);
+  var post = new AjaxPost(data, {
+    'spinner': new LoadingSpinner({
+      'reference_elm': button,
+      'in_parent': true,
+      'insert_method': 'prepend'
+    })
+  },
+  // Ajax replied
+  function(response){
     var json_response = JSON.parse(response);
-    $('#mailboxes-changepass-loader').fadeOut(300, function(){
-      $(this).remove();
-      if(json_response.status==1){
-        bootstrap_alert(json_response.status_message, 'success');
-        // Do callback, which is close the modal when validation is okay
-        if(typeof(success_callback)=='function') success_callback();
-      }
-      else {
-        bootstrap_alert(json_response.status_message, 'error');
-        if(typeof(failed_callback)=='function') failed_callback();
-      }
-    });
-  })
-  .error(function() {
-    $('#mailboxes-changepass-loader').fadeOut(200, function(){
-      $(this).remove();
-      bootstrap_alert("Connection error", 'error');
-    });
+    if(json_response.status==1){
+      bootstrap_alert(json_response.status_message, 'success');
+      if(typeof(success_callback)=='function') success_callback();
+    }
+    else {
+      bootstrap_alert(json_response.status_message, 'error');
+      if(typeof(failed_callback)=='function') failed_callback();
+    }
+  },
+  function(){
+    if(typeof(failed_callback)=='function') failed_callback();
   });
+  post.doAjaxPost();
 }
